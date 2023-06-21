7f63db672e848f449fa6b818d8c00cfc5e796355
Jiraphorz <jirakhuz@gmail.com>
2023-06-21 21:26:01 +0700

    e

--- ex1.ts
+++ ex1.ts
@@ -1,13 +1,38 @@
 export class Utils {
   public static findMax(...args: number[]): number {
+    // return Math.max(...args);
+    let max = args[0];
+      for (let i = 1; i < args.length; i++) {
+        if (args[i] > max) {
+          max = args[i];
+        }
+      }
+    return max;
     return Infinity
   }
 
   public static findMin(...args: number[]): number {
+    // return Math.min(...args);
+    let min = args[0];
+      for (let i = 1; i < args.length; i++) {
+        if (args[i] < min) {
+          min = args[i];
+        }
+      }
+    return min;
     return Infinity
   }
 
   public static reformatData(x: any): Record<string, any> {
+    const studentData = {};
+    x.forEach(item => {
+      const { name, role } = item;
+      if (!studentData[role]) {
+        studentData[role] = [];
+      }
+      studentData[role].push({ nickname: name });
+    });
+    return studentData
     return {}
   }
 }
--- ex2.ts
+++ ex2.ts
@@ -22,13 +22,59 @@ interface IStudentScore {
 }
 
 export const updateStudentScore = (store: IStore[], update: IStudentUpdateScore): IStore[] => {
+  const { name, scores } = update;
+  Object.keys(scores).forEach(subject => {
+    const subjectIndex = store.findIndex(item => item.subject === subject);
+    if (subjectIndex !== -1) {
+      const studentIndex = store[subjectIndex].students.findIndex(student => student.name === name);
+      if (studentIndex !== -1) {
+        store[subjectIndex].students[studentIndex].score = scores[subject];
+      } else {
+        store[subjectIndex].students.push({ name, score: scores[subject] });
+      }
+    } else {
+      store.push({ subject, students: [{ name, score: scores[subject] }] });
+    }
+  });
   return store
 }
 
 export const removeStudentScoreBySubject = (store: IStore[], record: IRemoveStudentScore): IStore[] => {
+  const {name, subject} = record;
+  const updatedStore = store.map(item => {
+    if (item.subject === subject) {
+      item.students = item.students.filter(student => student.name !== name);
+    }
+    return item;
+  });
+  return updatedStore
   return store
 }
 
 export const getStudentScoreBySubject = (store: IStore[], subjects: string[]): IStudentScore[] => {
+  const result = [];
+  subjects.forEach(subject => {
+    const data = store.find(data =>data.subject === subject);
+    if (data) {
+      data.students.forEach(student => {
+        let Student = result.find(s => s.name === student.name);
+        if (!Student) {
+          Student = { name: student.name };
+          result.push(Student);
+        }
+        Student[subject] = student.score;
+      });
+    }
+  });
+
+  result.forEach(student => {
+    subjects.forEach(subject => {
+      if (!student.hasOwnProperty(subject)) {
+        student[subject] = null;
+      }
+    });
+  });
+
+  return result
   return []
 }
